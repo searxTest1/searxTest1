d89b492fc9839f5002da27a09360dfb1e7a0def0
Mattias <searxTest1@gmail.com>
2018-11-26 16:34:09 +0100

    formated

--- keywordmonitor.py
+++ keywordmonitor.py
@@ -23,26 +23,18 @@ if not os.path.exists("keywords"):
 # Send email to you!
 #
 def send_alert(alert_email):
-    
     email_body = "The following are keyword hits that were just found:\r\n\r\n"
-    
     # walk through the searx results
     if alert_email.has_key("searx"):
-        
         for keyword in alert_email['searx']:
-            
             email_body += "\r\nKeyword: %s\r\n\r\n" % keyword
-            
             for keyword_hit in alert_email['searx'][keyword]:
-                
                 email_body += "%s\r\n" % keyword_hit
 
     '''
     # walk through pastebin results
     if alert_email.has_key("pastebin"):
-        
         for paste_id in alert_email['pastebin']:
-            
             email_body += "\r\nPastebin Link: https://pastebin.com/%s\r\n" % paste_id
             email_body += "Keywords:%s\r\n" % ",".join(alert_email['pastebin'][paste_id][0])
             email_body += "Paste Body:\r\n%s\r\n\r\n" % alert_email['pastebin'][paste_id][1]
@@ -55,72 +47,50 @@ def send_alert(alert_email):
     msg['To']      = alert_email_account
     
     server = smtplib.SMTP("smtp.gmail.com",587)
-    
     server.ehlo()
     server.starttls()
     server.login(alert_email_account,alert_email_password)
     server.sendmail(alert_email_account,alert_email_account,msg.as_string())
     server.quit()
-    
     print "[!] Alert email sent!"
-    
     return
 
 #
 # Check if the URL is new.
 #
 def check_urls(keyword,urls):
-    
     new_urls = []
-    
     if os.path.exists("keywords/%s.txt" % keyword):
-        
         with open("keywords/%s.txt" % keyword,"r") as fd:
-            
             stored_urls = fd.read().splitlines()
-        
         for url in urls:
-            
             if url not in stored_urls:
-                
                 print "[*] New URL for %s discovered: %s" % (keyword,url)
-                
                 new_urls.append(url)
-                
     else:
-        
         new_urls = urls
         
     # now store the new urls back in the file
     with open("keywords/%s.txt" % keyword,"ab") as fd:
-        
         for url in new_urls:
             fd.write("%s\r\n" % url)
-            
-    
     return new_urls
-
 	
 #
 # Poll Searx instance for keyword.
 #
 def check_searx(keyword):
-    
     hits = []
-    
     # build parameter dictionary
     params               = {}
     params['q']          = keyword
     params['categories'] = 'general'
     params['time_range'] = 'year' #day,week,month or year will work
     params['format']     = 'json'
-    
     print "[*] Querying Searx for: %s" % keyword
-    
     # send the request off to searx
     try:
-        response = requests.get(searx_url,params=params)
-        
+        response = requests.get(searx_url,params=params)    
         results  = response.json()
     except:
         return hits
@@ -129,25 +99,18 @@ def check_searx(keyword):
     if len(results['results']):
         print "found results"
         urls = []
-        
         for result in results['results']:
-            
             if result['url'] not in urls:
-            
                 urls.append(result['url'])
-            
         hits = check_urls(keyword,urls)
-    
     return hits
 	
 #
 # Check Pastebin for keyword list.
 #
-def check_pastebin(keywords):
-    
+def check_pastebin(keywords):    
     new_ids    = []
     paste_hits = {}
-    
     # poll the Pastebin API
     try:
         response = requests.get("http://pastebin.com/api_scraping.php?limit=500")
@@ -156,7 +119,6 @@ def check_pastebin(keywords):
     
     # parse the JSON
     result   = response.json()
-    
     # load up our list of stored paste ID's and only check the new ones
     if os.path.exists("pastebin_ids.txt"):
         with open("pastebin_ids.txt","rb") as fd:
@@ -165,55 +127,39 @@ def check_pastebin(keywords):
         pastebin_ids = []
         
     for paste in result:
-    
         if paste['key'] not in pastebin_ids:
-            
             new_ids.append(paste['key'])
-    
             # this is a new paste so send a secondary request to retrieve
             # it and then check it for our keywords 
             paste_response       = requests.get(paste['scrape_url'])
             paste_body_lower     = paste_response.content.lower()
-            
             keyword_hits = []
-            
             for keyword in keywords:
-                
                 if keyword.lower() in paste_body_lower:
                     keyword_hits.append(keyword)
                 
             if len(keyword_hits):      
                 paste_hits[paste['key']] = (keyword_hits,paste_response.content)
-            
                 print "[*] Hit on Pastebin for %s: %s" % (str(keyword_hits),paste['full_url'])
  
     # store the newly checked IDs 
     with open("pastebin_ids.txt","ab") as fd:
-        
         for pastebin_id in new_ids:
-            
             fd.write("%s\r\n" % pastebin_id)
     
     print "[*] Successfully processed %d Pastebin posts." % len(new_ids)
-    
     return paste_hits
 	
 def check_keywords(keywords):
     alert_email          = {}
-    
     time_start = time.time()
-    
     # use the list of keywords and check each against searx
     for keyword in keywords:
-        
         # query searx for the keyword
         result = check_searx(keyword)
-        
         if len(result):
-            
             if not alert_email.has_key("searx"):
                 alert_email['searx'] = {}            
-            
             alert_email['searx'][keyword] = result
 
     '''
@@ -247,10 +193,7 @@ def check_keywords(keywords):
  
 # now perform the main loop
 while True:
-    
     alert_email = check_keywords(keywords)
-    
     if len(alert_email.keys()):
-        
         # if we have alerts send them out
         send_alert(alert_email)
